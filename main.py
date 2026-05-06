16fbc815885837fe0f150b86394f4b0e4af1aacf
tibertun <tominasonyaa@gmail.com>
2026-05-06 16:46:30 +0300

    first step

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame,sys
+pygame.init()
+screen = pygame.display.set_mode((300, 600))
+screen.fill((0, 0, 0))
+pygame.display.set_caption("Python Tetris")
+clock = pygame.time.Clock()
+#головний цикл гри
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(60)
