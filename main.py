500456e439f4488be001f3d233be08b414fce1c4
tibertun <tominasonyaa@gmail.com>
2026-05-08 16:03:04 +0300

    proverka

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ pygame.display.set_caption("Python Tetris")
 clock = pygame.time.Clock()
 
 game_grid = Grid()
+
+# game_grid.grid[0][0] = 1
+# game_grid.grid[3][5] = 4
+# game_grid.grid[17][8] = 7
+
 game_grid.print_grid()
 
 #головний цикл гри
